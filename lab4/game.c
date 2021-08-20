219bf6d8195ad300ad08384d7f1bb88eaed2379a
Nick Wang <hwan0109@student.monash.edu>
2021-08-20 13:40:52 +1000

    lab 4

--- lab4/game.c
+++ lab4/game.c
@@ -1,1 +1,40 @@
-int main() {}
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define chunksize 2
+#define minN 1
+#define maxN 5
+#define N (maxN - minN + 1)
+#define SIZE 10
+
+int digits[SIZE];
+
+int main() {
+    omp_set_dynamic(0);
+    omp_set_num_threads(4);
+
+#pragma omp parallel for schedule(static, chunksize)
+    for (int i = 0; i < SIZE; i++) {
+        srand(time(NULL) ^ i ^ omp_get_thread_num());
+        int r = rand() % (N) + minN;
+        digits[i] = r;
+    }
+
+    int count[maxN + 1] = {0};
+    for (int i = 0; i < SIZE; i++) count[digits[i]]++;
+
+    for (int i = 0; i < SIZE; i++) printf("%i ", digits[i]);
+    printf("\n");
+    for (int i = 0; i < maxN + 1; i++) printf("%i ", count[i]);
+    printf("\n");
+
+    int win = 0;
+#pragma omp parallel for reduction(+ : win) schedule(static, chunksize)
+    for (int i = 0; i < maxN + 1; i++) {
+        if (count[i] > 1) win++;
+    }
+
+    printf("Wins: %i", win);
+}
