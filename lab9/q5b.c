44ad0af06f3226f0caef518044b65619277c988d
Nick Wang <hwan0109@student.monash.edu>
2021-09-26 15:54:17 +1000

    t5

--- lab9/q5b.c
+++ lab9/q5b.c
@@ -12,6 +12,7 @@
 #include <time.h>
 #define SHIFT_ROW 0
 #define SHIFT_COL 1
+#define SHIFT_DEP 2
 #define DISP 1
 // Set to a small value so we get more collisions
 #define MAX_PRIME 10
@@ -31,25 +32,27 @@ int is_prime(int p) {
 }
 
 int main(int argc, char *argv[]) {
-    int ndims = 2, size, my_rank, reorder, my_cart_rank, ierr;
-    int nrows, ncols;
+    int ndims = 3, size, my_rank, reorder, my_cart_rank, ierr;
+    int nrows, ncols, ndepth;
     int nbr_i_lo, nbr_i_hi;
     int nbr_j_lo, nbr_j_hi;
-    MPI_Comm comm2D;
+    int nbr_k_lo, nbr_k_hi;
+    MPI_Comm comm3D;
     int dims[ndims], coord[ndims];
     int wrap_around[ndims];
-
     /* start up initial MPI environment */
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     /* process command line arguments*/
-    if (argc == 3) {
+    if (argc == 4) {
         nrows = atoi(argv[1]);
         ncols = atoi(argv[2]);
-        dims[0] = nrows; /* number of rows */
-        dims[1] = ncols; /* number of columns */
-        if ((nrows * ncols) != size) {
+        ndepth = atoi(argv[3]);
+        dims[0] = nrows;  /* number of rows */
+        dims[1] = ncols;  /* number of columns */
+        dims[2] = ndepth; /* magnitude of depth */
+        if ((nrows * ncols * ndepth) != size) {
             if (my_rank == 0)
                 printf(
                     "ERROR: nrows*ncols)=%d *%d = %d != %d\n", nrows, ncols, nrows * ncols, size);
@@ -57,8 +60,8 @@ int main(int argc, char *argv[]) {
             return 0;
         }
     } else {
-        nrows = ncols = (int) sqrt(size);
-        dims[0] = dims[1] = 0;
+        nrows = ncols = ndepth = (int) cbrt(size);
+        dims[0] = dims[1] = dims[2] = 0;
     }
     /************************************************************
      */
@@ -74,25 +77,24 @@ int main(int argc, char *argv[]) {
             dims[0],
             dims[1]);
     /* create cartesian mapping */
-    wrap_around[0] = wrap_around[1] = 0; /* periodic shift is.false. */
+    wrap_around[0] = wrap_around[1] = wrap_around[2] = 0; /* periodic shift is.false. */
     reorder = 1;
     ierr = 0;
-    ierr = MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, wrap_around, reorder, &comm2D);
+    ierr = MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, wrap_around, reorder, &comm3D);
     if (ierr != 0) printf("ERROR[%d] creating CART\n", ierr);
-
     /* find my coordinates in the cartesian communicator group */
-    MPI_Cart_coords(comm2D, my_rank, ndims, coord);
+    MPI_Cart_coords(comm3D, my_rank, ndims, coord);
     /* use my cartesian coordinates to find my rank in cartesian
     group*/
-    MPI_Cart_rank(comm2D, coord, &my_cart_rank);
-
+    MPI_Cart_rank(comm3D, coord, &my_cart_rank);
     /* get my neighbors; axis is coordinate dimension of shift */
     /* axis=0 ==> shift along the rows: P[my_row-1]: P[me] :
     P[my_row+1] */
     /* axis=1 ==> shift along the columns P[my_col-1]: P[me] :
     P[my_col+1] */
-    MPI_Cart_shift(comm2D, SHIFT_ROW, DISP, &nbr_i_lo, &nbr_i_hi);
-    MPI_Cart_shift(comm2D, SHIFT_COL, DISP, &nbr_j_lo, &nbr_j_hi);
+    MPI_Cart_shift(comm3D, SHIFT_ROW, DISP, &nbr_i_lo, &nbr_i_hi);
+    MPI_Cart_shift(comm3D, SHIFT_COL, DISP, &nbr_j_lo, &nbr_j_hi);
+    MPI_Cart_shift(comm3D, SHIFT_DEP, DISP, &nbr_k_lo, &nbr_k_hi);
 
     /* Process communication */
     // Open file
@@ -112,49 +114,60 @@ int main(int argc, char *argv[]) {
         }
 
         // MPI outputs
-        MPI_Request send_request[4];
-        MPI_Request receive_request[4];
-        MPI_Status send_status[4];
-        MPI_Status receive_status[4];
+        MPI_Request send_request[6];
+        MPI_Request receive_request[6];
+        MPI_Status send_status[6];
+        MPI_Status receive_status[6];
 
         // Send primes to adjacent
-        MPI_Isend(&randomPrime, 1, MPI_INT, nbr_i_lo, 0, comm2D, &send_request[0]);
-        MPI_Isend(&randomPrime, 1, MPI_INT, nbr_i_hi, 0, comm2D, &send_request[1]);
-        MPI_Isend(&randomPrime, 1, MPI_INT, nbr_j_lo, 0, comm2D, &send_request[2]);
-        MPI_Isend(&randomPrime, 1, MPI_INT, nbr_j_hi, 0, comm2D, &send_request[3]);
+        MPI_Isend(&randomPrime, 1, MPI_INT, nbr_i_lo, 0, comm3D, &send_request[0]);
+        MPI_Isend(&randomPrime, 1, MPI_INT, nbr_i_hi, 0, comm3D, &send_request[1]);
+        MPI_Isend(&randomPrime, 1, MPI_INT, nbr_j_lo, 0, comm3D, &send_request[2]);
+        MPI_Isend(&randomPrime, 1, MPI_INT, nbr_j_hi, 0, comm3D, &send_request[3]);
+        MPI_Isend(&randomPrime, 1, MPI_INT, nbr_k_lo, 0, comm3D, &send_request[4]);
+        MPI_Isend(&randomPrime, 1, MPI_INT, nbr_k_hi, 0, comm3D, &send_request[5]);
 
         // Receive primes from adjacent
-        int recvValL = -1, recvValR = -1, recvValT = -1, recvValB = -1;
-        MPI_Irecv(&recvValT, 1, MPI_INT, nbr_i_lo, 0, comm2D, &receive_request[0]);
-        MPI_Irecv(&recvValB, 1, MPI_INT, nbr_i_hi, 0, comm2D, &receive_request[1]);
-        MPI_Irecv(&recvValL, 1, MPI_INT, nbr_j_lo, 0, comm2D, &receive_request[2]);
-        MPI_Irecv(&recvValR, 1, MPI_INT, nbr_j_hi, 0, comm2D, &receive_request[3]);
+        int receive_values[6];
+        MPI_Irecv(&receive_values[0], 1, MPI_INT, nbr_i_lo, 0, comm3D, &receive_request[0]);
+        MPI_Irecv(&receive_values[1], 1, MPI_INT, nbr_i_hi, 0, comm3D, &receive_request[1]);
+        MPI_Irecv(&receive_values[2], 1, MPI_INT, nbr_j_lo, 0, comm3D, &receive_request[2]);
+        MPI_Irecv(&receive_values[3], 1, MPI_INT, nbr_j_hi, 0, comm3D, &receive_request[3]);
+        MPI_Irecv(&receive_values[4], 1, MPI_INT, nbr_k_lo, 0, comm3D, &receive_request[4]);
+        MPI_Irecv(&receive_values[5], 1, MPI_INT, nbr_k_hi, 0, comm3D, &receive_request[5]);
 
-        MPI_Waitall(4, send_request, send_status);
-        MPI_Waitall(4, receive_request, receive_status);
+        MPI_Waitall(6, send_request, send_status);
+        MPI_Waitall(6, receive_request, receive_status);
 
         /* Check and write primes to file */
-        if (recvValT == randomPrime) {
+        if (receive_values[0] == randomPrime) {
             printf("Rank %d == Rank %d on %d\n", my_rank, nbr_i_lo, randomPrime);
             fprintf(fid, "Rank %d == Rank %d on %d\n", my_rank, nbr_i_lo, randomPrime);
         }
-        if (recvValB == randomPrime) {
+        if (receive_values[1] == randomPrime) {
             printf("Rank %d == Rank %d on %d\n", my_rank, nbr_i_hi, randomPrime);
             fprintf(fid, "Rank %d == Rank %d on %d\n", my_rank, nbr_i_hi, randomPrime);
         }
-        if (recvValL == randomPrime) {
+        if (receive_values[2] == randomPrime) {
             printf("Rank %d == Rank %d on %d\n", my_rank, nbr_j_lo, randomPrime);
             fprintf(fid, "Rank %d == Rank %d on %d\n", my_rank, nbr_j_lo, randomPrime);
         }
-        if (recvValR == randomPrime) {
+        if (receive_values[3] == randomPrime) {
             printf("Rank %d == Rank %d on %d\n", my_rank, nbr_j_hi, randomPrime);
             fprintf(fid, "Rank %d == Rank %d on %d\n", my_rank, nbr_j_hi, randomPrime);
         }
+        if (receive_values[4] == randomPrime) {
+            printf("Rank %d == Rank %d on %d\n", my_rank, nbr_k_lo, randomPrime);
+            fprintf(fid, "Rank %d == Rank %d on %d\n", my_rank, nbr_k_lo, randomPrime);
+        }
+        if (receive_values[5] == randomPrime) {
+            printf("Rank %d == Rank %d on %d\n", my_rank, nbr_k_hi, randomPrime);
+            fprintf(fid, "Rank %d == Rank %d on %d\n", my_rank, nbr_k_hi, randomPrime);
+        }
     }
 
-    fclose(fid);
     fflush(stdout);
-    MPI_Comm_free(&comm2D);
+    MPI_Comm_free(&comm3D);
     MPI_Finalize();
     return 0;
 }
